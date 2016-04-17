ac2dd9cc361e687bd3489b52792adb6387caef4d
karthikrao75 <karthik.prampalli@gmail.com>
2016-04-17 22:31:51 +0530

    completed

--- FirstHibernateDemo/src/com/karthik/FirstHibernateDemo/TesterPackage/Tester.java
+++ FirstHibernateDemo/src/com/karthik/FirstHibernateDemo/TesterPackage/Tester.java
@@ -1,5 +1,9 @@
 package com.karthik.FirstHibernateDemo.TesterPackage;
 
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
@@ -10,16 +14,35 @@ public class Tester {
 
 	public static void main(String []args){
 		Userdetails userdetails=new Userdetails();
-		userdetails.setUserId(1);
+		
 		userdetails.setUserName("karthik");
-		Configuration cfg=new Configuration().configure("/Resource/HibernateResource/hibernate.cfg.xml");
-		SessionFactory factory=cfg.buildSessionFactory();
-		Session s=factory.openSession();
+		Configuration cfg;
+		SessionFactory factory;
+		Session s=null;
+		try{
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MM-yyyy");
+		Date d=dateFormat.parse("07-05-1992");
+	System.out.println(d.toString());
+		userdetails.setDateOfBirth(d);
+		cfg=new Configuration().configure("/Resource/HibernateResource/hibernate.cfg.xml");
+		factory=cfg.buildSessionFactory();
+		s=factory.openSession();
 		s.getTransaction();
 		s.save(userdetails);
 		s.beginTransaction().commit();
-		s.close();
 		System.out.println("operation completed");
+		}
+		catch(ParseException exception){
+			exception.printStackTrace();
+		}
+		finally {
+			if(s!=null){
+				s.close();
+			}
+			
+		}
+		
+		
 		
 	}
 }
