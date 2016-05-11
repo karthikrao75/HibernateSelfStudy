a4cfaea57a5ca312dcafcb2f3d5ea089b85178de
karthikrao75 <karthik.prampalli@gmail.com>
2016-05-11 20:49:34 +0530

    initial commit

---
+++ NamedQueryHibernateDemo/src/com/karthik/NamedQueryHibernateDemo/HibernateEntity/Userdetails.java
@@ -0,0 +1,96 @@
+package com.karthik.NamedQueryHibernateDemo.HibernateEntity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+@Table(name = "User_DETAILS")
+@NamedQuery(name="userDetails.byName",query="from Userdetails where userName = ? ")
+@NamedQueries(value={@NamedQuery(name="userDetails.byID",query="from Userdetails where userId=?"),
+					@NamedQuery(name="userDetails",query="from Userdetails")})
+public class Userdetails {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="USER_ID")
+	private int userId;
+	@Column(name="USER_NAME")
+	private String userName;
+	@Temporal(TemporalType.DATE)
+	private Date dateOfBirth;
+	@Transient
+	private int age;
+
+	public Userdetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public Userdetails(int userId, String userName, Date dateOfBirth, int age) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.dateOfBirth = dateOfBirth;
+		this.age = age;
+	}
+
+
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "Userdetails [userId=" + userId + ", userName=" + userName + "]";
+	}
+
+}
---
+++ NamedQueryHibernateDemo/src/com/karthik/NamedQueryHibernateDemo/TesterPackage/Tester.java
@@ -0,0 +1,61 @@
+package com.karthik.NamedQueryHibernateDemo.TesterPackage;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.karthik.NamedQueryHibernateDemo.HibernateEntity.Userdetails;
+
+public class Tester {
+
+	public static void main(String []args){
+		Userdetails userdetails=new Userdetails();
+		
+		userdetails.setUserName("karthik");
+		//object is in transient state
+		Configuration cfg;
+		SessionFactory factory;
+		Session s=null;
+		try{
+		SimpleDateFormat dateFormat=new SimpleDateFormat("dd-MM-yyyy");
+		Date d=dateFormat.parse("07-05-1992");
+	System.out.println(d.toString());
+		userdetails.setDateOfBirth(d);
+		cfg=new Configuration().configure("/Resource/HibernateResource/hibernate.cfg.xml");
+		factory=cfg.buildSessionFactory();
+		s=factory.openSession();
+		s.getTransaction();
+		s.save(userdetails);
+		//persistence state
+		s.beginTransaction().commit();
+		System.out.println("operation completed");
+		
+		Query q=s.getNamedQuery("userDetails.byName");
+		q.setString(0, "karthik");
+		Userdetails u=(Userdetails)q.uniqueResult();
+		System.out.println(u);
+		
+		Query q1=s.getNamedQuery("userDetails.byID");
+		q1.setInteger(0, 1);
+		Userdetails u1=(Userdetails)q.uniqueResult();
+		System.out.println(u1);
+		}
+		catch(ParseException exception){
+			exception.printStackTrace();
+		}
+		finally {
+			if(s!=null){
+				s.close();
+			}
+			
+		}
+		
+		
+		
+	}
+}
