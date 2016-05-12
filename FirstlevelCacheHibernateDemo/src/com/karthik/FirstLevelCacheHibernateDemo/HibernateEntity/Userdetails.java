6f40f8b3ebd35eaf85b646529c95dfcf32ade5c1
karthikrao75 <karthik.prampalli@gmail.com>
2016-05-12 21:57:36 +0530

    commit

---
+++ FirstlevelCacheHibernateDemo/src/com/karthik/FirstLevelCacheHibernateDemo/HibernateEntity/Userdetails.java
@@ -0,0 +1,95 @@
+package com.karthik.FirstLevelCacheHibernateDemo.HibernateEntity;
+
+import java.util.Date;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.Temporal;
+import javax.persistence.TemporalType;
+import javax.persistence.Transient;
+
+@Entity
+// (name="U_DETAILS")
+@Table(name = "User_DETAILS")
+// if both have name then @table is given preference.
+public class Userdetails {
+	@Id
+	@GeneratedValue(strategy=GenerationType.AUTO)
+	@Column(name="USER_ID")
+	private int userId;
+	@Column(name="USER_NAME")
+	private String userName;
+	@Temporal(TemporalType.DATE)
+	//default temporal type is TIMESTAMP.
+	private Date dateOfBirth;
+	@Transient
+	//this variable will be ignored while creating table.
+	private int age;
+
+	public Userdetails() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	
+
+	public Userdetails(int userId, String userName, Date dateOfBirth, int age) {
+		super();
+		this.userId = userId;
+		this.userName = userName;
+		this.dateOfBirth = dateOfBirth;
+		this.age = age;
+	}
+
+
+
+	public Date getDateOfBirth() {
+		return dateOfBirth;
+	}
+
+
+
+	public void setDateOfBirth(Date dateOfBirth) {
+		this.dateOfBirth = dateOfBirth;
+	}
+
+
+
+	public int getAge() {
+		return age;
+	}
+
+
+
+	public void setAge(int age) {
+		this.age = age;
+	}
+
+
+
+	public int getUserId() {
+		return userId;
+	}
+
+	public void setUserId(int userId) {
+		this.userId = userId;
+	}
+
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	@Override
+	public String toString() {
+		return "Userdetails [userId=" + userId + ", userName=" + userName + "]";
+	}
+
+}
